45e211a0f7bdecb44a75982d88c83d7cfee293db
Vasan Subramanian <vasan.srini@gmail.com>
2016-08-08 14:24:48 +0530

    Create API

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.static('static'));
+app.use(bodyParser.json());
 
 const issues = [
   {
@@ -19,6 +21,18 @@ const issues = [
 app.get('/api/issues', (req, res) => {
   const metadata = { total_count: issues.length };
   res.json({ _metadata: metadata, records: issues });
+});
+
+app.post('/api/issues', (req, res) => {
+  const newIssue = req.body;
+  newIssue.id = issues.length + 1;
+  newIssue.created = new Date();
+  if (!newIssue.status)
+    newIssue.status = 'New';
+
+  issues.push(newIssue);
+
+  res.json(newIssue);
 });
 
 app.listen(3000, () => {
