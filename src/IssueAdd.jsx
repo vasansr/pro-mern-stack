3dcf2777bd81d0b41ecb3d7a04743d9f2bbc7621
Vasan Subramanian <vasan.srini@gmail.com>
2016-11-17 15:20:54 +0530

    Forms - Inline Forms

--- src/IssueAdd.jsx
+++ src/IssueAdd.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 export default class IssueAdd extends React.Component {
   constructor() {
@@ -22,11 +23,13 @@ export default class IssueAdd extends React.Component {
   render() {
     return (
       <div>
-        <form name="issueAdd" onSubmit={this.handleSubmit}>
-          <input type="text" name="owner" placeholder="Owner" />
-          <input type="text" name="title" placeholder="Title" />
-          <button>Add</button>
-        </form>
+        <Form inline name="issueAdd" onSubmit={this.handleSubmit}>
+          <FormControl name="owner" placeholder="Owner" />
+          {' '}
+          <FormControl name="title" placeholder="Title" />
+          {' '}
+          <Button type="submit" bsStyle="primary">Add</Button>
+        </Form>
       </div>
     );
   }
