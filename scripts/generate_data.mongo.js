7ab8af10546d12c39ac8e3a42be46a71dca13692
Vasan Subramanian <vasan.srini@gmail.com>
2016-11-30 11:19:32 +0530

    MongoDB Aggregate: Minor formatting changes

--- scripts/generate_data.mongo.js
+++ scripts/generate_data.mongo.js
@@ -7,8 +7,10 @@ var statuses = ['New','Open','Assigned','Fixed','Verified','Closed'];
 
 var i;
 for (i=0; i<1000; i++) {
-  var randomCreatedDate = new Date((new Date()) - Math.floor(Math.random() * 60) * 1000*60*60*24);
-  var randomCompletionDate = new Date((new Date()) - Math.floor(Math.random() * 60) * 1000*60*60*24);
+  var randomCreatedDate = new Date(
+    (new Date()) - Math.floor(Math.random() * 60) * 1000*60*60*24);
+  var randomCompletionDate = new Date(
+    (new Date()) - Math.floor(Math.random() * 60) * 1000*60*60*24);
   var randomOwner = owners[Math.floor(Math.random() * 6)];
   var randomStatus = statuses[Math.floor(Math.random() * 6)];
   var randomEffort = Math.ceil(Math.random() * 20);
