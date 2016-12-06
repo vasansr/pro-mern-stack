ca1aa0dc0f1af1fccd88e9bb840f64336793c084
Vasan Subramanian <vasan.srini@gmail.com>
2016-12-06 16:06:37 +0530

    Google Sign-In: moved config to pure JavaScript

---
+++ static/config.sample.js
@@ -0,0 +1,8 @@
+/*
+ * This is a sample, with a Client ID that is not meant for public use. Please
+ * copy this file as config.js and use your own Google Client ID for your app.
+ */
+window.config = {
+  googleClientId = '848619354847-uqpdvmuoqusmi92dh0p2ouhd9m9nt22k.apps.googleusercontent.com',
+};
+
